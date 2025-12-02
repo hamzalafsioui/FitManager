13266b9e2517a4d777d9b95bf7675b144ecabc18
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-02 23:26:37 +0100

    getAllCategories & getTotalCourses

--- includes/functions_courses.php
+++ includes/functions_courses.php
@@ -4,7 +4,6 @@ require_once __DIR__ . "/../db.php";
 
 function getAllCourses(){
     global $pdo;
-    global $name;
     $sql = "SELECT c.*,cat.name As category_name FROM courses c JOIN categories cat ON c.category_id = cat.id ORDER BY c.course_date ASC";
 
     RETURN $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
@@ -63,6 +62,23 @@ function updateCourseById($id, $data) {
     ]);
 }
 
+function getAllCategories(){
+    global $pdo;
+    return  $pdo->query("SELECT * FROM categories")->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getTotalCourses(){
+    global $pdo;
+    $stmt =  $pdo->query("SELECT count(*) AS total_courses FROM courses");
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result["total_courses"];
+
+}
+
+
+
+$count = getTotalCourses();
+// echo $count;
 
 
 $customVariable = getAllCourses();
@@ -86,8 +102,9 @@ $insertedData = [
 // $deleteCourse = deleteCourse(4);
 // echo $deleteCourse;
 
-$updatedData = updateCourseById(3,$insertedData);
-echo $updatedData;
+// $updatedData = updateCourseById(3,$insertedData);
+// echo $updatedData;
 
-
+// $categories = getAllCategories();
+// print_r($categories);
 ?>
