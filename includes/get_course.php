a4ad2b7087fd8cb7a36902108ec67fe5b98ed227
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-03 23:56:33 +0100

    get course

---
+++ includes/get_course.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . "/functions_data/functions_courses.php";
+
+header('Content-Type: application/json');
+
+if (!isset($_GET['id'])) {
+    echo json_encode(["status"=>"error","message"=>"No ID provided"]);
+    exit;
+}
+
+$id = intval($_GET['id']);
+$course = getCourseById($id);
+
+if ($course) {
+    echo json_encode(["status"=>"success","data"=>$course]);
+} else {
+    echo json_encode(["status"=>"error","message"=>"Course not found"]);
+}
