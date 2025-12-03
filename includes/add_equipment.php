60ba1de73d98d4cca08d0e1a97f67f4868d17e54
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-04 00:44:38 +0100

    fix add equipment

--- includes/add_equipment.php
+++ includes/add_equipment.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         echo json_encode([
             "status" => "success",
-            "data" => $equipment
+            "equipment" => $equipment
         ]);
         exit;
     }
