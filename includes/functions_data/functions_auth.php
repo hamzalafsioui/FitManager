aa69dc3d8af66f048dd426c3835dc2a402a2f8fc
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-10 20:52:04 +0100

    implement functions_auth

---
+++ includes/functions_data/functions_auth.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . "/../../db.php";
+
+
+function getUserByEmail(string $email) {
+    global $pdo;
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+
+function createUser(string $email, string $password, string $first_name, string $last_name, ?string $gender, ?int $age, int $role_id = 2) {
+    global $pdo;
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $pdo->prepare("
+        INSERT INTO users (email, password_hash, role_id, first_name, last_name, gender, age)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ");
+
+    return $stmt->execute([
+        $email,
+        $hash,
+        $role_id,
+        $first_name,
+        $last_name,
+        $gender,
+        $age
+    ]);
+}
+
+
+function loginUser(string $email, string $password) {
+    $user = getUserByEmail($email);
+
+    if ($user && password_verify($password, $user['password_hash'])) {
+        return $user;
+    }
+
+    return false;
+}
+
